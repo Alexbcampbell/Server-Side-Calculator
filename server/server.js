38f515efe245b52abfeb4f98c4a22cabd2335738
Alex Campbell <alexbe.campbell@gmail.com>
2020-10-17 10:56:38 -0500

    added math functions to server.js

--- server/server.js
+++ server/server.js
@@ -7,6 +7,22 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+function addition(numOne, numTwo) {
+  return numOne + numTwo;
+}
+
+function subtraction(numOne, numTwo) {
+  return numOne - numTwo;
+}
+
+function multiply(numOne, numTwo) {
+  return numOne * numTwo;
+}
+
+function divide(numOne, numTwo) {
+  return numOne / numTwo;
+}
+
 app.post('/calculate', (req, res) => {
   console.log(req.body);
   res.sendStatus(200);
